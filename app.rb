187132ff8a68c1fbfae6f3f2943aa06cf9f4e85e
elicoon <elicoon@gmail.com>
2020-03-19 18:38:22 +0000

    saving

--- app.rb
+++ app.rb
@@ -23,6 +23,12 @@ before do
     @current_user = users_table.where(id: session["user_id"]).to_a[0]
 end
 
+
+get '/send_text' do
+
+end
+
+
 # homepage and list of events (aka "index")
 get "/" do
     puts "params: #{params}"
@@ -78,8 +84,21 @@ end
 
 post "/rsvps/:id/update" do
     puts "params: #{params}"
+    
+    @rsvp = rsvps_table.where(id: params["id"]).to_a[0]
+    @event = events_table.where(id: @rsvp[:event_id]).to_a[0]
+    
+    if @current_user && @current_user[:id] == @rsvp[:id]
+        rsvps_table.where(id: params["id"]).update(
+            comments: params["comments"], 
+            going: params["going"]
+            )
+        view "update_rsvp"
+    else 
+        view "error"
+    end
 
-    view "update_rsvp"
+    
 end
 
 get "/rsvps/:id/destroy" do
@@ -102,12 +121,18 @@ end
 post "/users/create" do
     puts "params: #{params}"
 
-    users_table.insert(
-        name: params["name"],
-        email: params["email"],
-        password: BCrypt::Password.create(params["password"])
-    )
-    view "create_user"
+    #if user with this email already exists, throw an error
+    existing_user = users_table.where(email: params["email"]).to_a[0]
+    if existing_user
+        view "error"
+    else
+        users_table.insert(
+            name: params["name"],
+            email: params["email"],
+            password: BCrypt::Password.create(params["password"])
+        )
+        view "create_user"
+    end
 end
 
 # display the login form (aka "new")
@@ -139,5 +164,5 @@ end
 get "/logout" do
     # remove encrypted cookie for logged out user
     session["user_id"] = nil
-    view "logout"
+    redirect "/logins/new"
 end
